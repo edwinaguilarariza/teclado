01bfd40a74235a7ccc631101ef981223797ad530
edwinaguilarariza <edwinaguilarariza1@yahoo.com>
2021-03-18 15:12:21 -0500

    segunda subida

---
+++ reto.js
@@ -0,0 +1,365 @@
+let casillas = document.getElementById("casillas");
+let historial = document.getElementById("historial");
+let mensajes = document.getElementById("mensajes");
+
+let btnNum1 = document.getElementById("btnNum1");
+let btnNum2 = document.getElementById("btnNum2");
+let btnNum3 = document.getElementById("btnNum3");
+let btnNum4 = document.getElementById("btnNum4");
+let btnNum5 = document.getElementById("btnNum5");
+let btnNum6 = document.getElementById("btnNum6");
+let btnNum7 = document.getElementById("btnNum7");
+let btnNum8 = document.getElementById("btnNum8");
+let btnNum9 = document.getElementById("btnNum9");
+let btnBorrar = document.getElementById("btnBorrar");
+
+let btnLetrasQ = document.getElementById("btnLetrasQ");
+let btnLetrasW = document.getElementById("btnLetrasW");
+let btnLetrasE = document.getElementById("btnLetrasE");
+let btnLetrasR = document.getElementById("btnLetrasR");
+let btnLetraT = document.getElementById("btnLetrasT");
+let btnLetrasY = document.getElementById("btnLetrasY");
+let btnLetrasI = document.getElementById("btnLetrasI");
+let btnLetrasO = document.getElementById("btnLetrasO");
+let btnLetrasP = document.getElementById("btnLetrasP");
+let btnEnter = document.getElementById("btnEnter");
+
+let btnLetrasA = document.getElementById("btnLetrasA");
+let btnLetrasS = document.getElementById("btnLetrasS");
+let btnLetrasD = document.getElementById("btnLetrasD");
+let btnLetrasF = document.getElementById("btnLetrasF");
+let btnLetrasG = document.getElementById("btnLetrasG");
+let btnLetrasH = document.getElementById("btnLetrasH");
+let btnLetrasJ = document.getElementById("btnLetrasJ");
+let btnLetrasK = document.getElementById("btnLetrasK");
+let btnLetrasL = document.getElementById("btnLetrasL");
+let btnLetrasN_ = document.getElementById("btnLetrasN_");
+
+let btnLetrasZ = document.getElementById("btnLetrasA");
+let btnLetrasX = document.getElementById("btnLetrasA");
+let btnLetrasC = document.getElementById("btnLetrasA");
+let btnLetrasV = document.getElementById("btnLetrasA");
+let btnLetrasB = document.getElementById("btnLetrasA");
+let btnLetrasN = document.getElementById("btnLetrasA");
+let btnLetrasM = document.getElementById("btnLetrasA");
+let btnLetrasComa = document.getElementById("btnLetrasComa");
+let btnLetrasPuntoYComa = document.getElementById("btnLetrasPuntoYComa");
+let btnLetrasPunto = document.getElementById("btnLetrasPunto");
+let btnLetrasGuion = document.getElementById("btnLetrasGuion");
+
+let numerosTemp = "";
+
+let letrasTemp = "";
+let historialTemp = "";
+
+const btnNum1H = () => {
+  if (numerosTemp === "") {
+    casillas.innerHTML = numerosTemp + "1";
+    historial.innerHTML = historialTemp + "1";
+    historialTemp = historialTemp + "1";
+    console.log(numerosTemp);
+  }
+};
+
+const btnNum2H = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "2";
+      historial.innerHTML = historialTemp + "2";
+      historialTemp = historialTemp + "2";
+      console.log(numerosTemp);
+    }
+};
+
+const btnNum3H = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "3";
+      historial.innerHTML = historialTemp + "3";
+      historialTemp = historialTemp + "3";
+      console.log(numerosTemp);
+    }
+};
+
+const btnNum4H = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "4";
+      historial.innerHTML = historialTemp + "4";
+      historialTemp = historialTemp + "4";
+      console.log(numerosTemp);
+    }
+  };
+
+  const btnNum5H = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "5";
+      historial.innerHTML = historialTemp + "5";
+      historialTemp = historialTemp + "5";
+      console.log(numerosTemp);
+    }
+  };
+
+  const btnNum6H = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "6";
+      historial.innerHTML = historialTemp + "6";
+      historialTemp = historialTemp + "6";
+      console.log(numerosTemp);
+    }
+  };
+
+  const btnNum7H = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "7";
+      historial.innerHTML = historialTemp + "7";
+      historialTemp = historialTemp + "7";
+      console.log(numerosTemp);
+    }
+  };
+
+  const btnNum8H = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "8";
+      historial.innerHTML = historialTemp + "8";
+      historialTemp = historialTemp + "8";
+      console.log(numerosTemp);
+    }
+  };
+
+  const btnNum9H = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "9";
+      historial.innerHTML = historialTemp + "9";
+      historialTemp = historialTemp + "9";
+      console.log(numerosTemp);
+    }
+  };
+  
+  //letras
+
+  const btnLetrasQH = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "q";
+      historial.innerHTML = historialTemp + "q";
+      historialTemp = historialTemp + "q";
+      console.log(numerosTemp);
+    }
+};
+const btnLetrasWH = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "w";
+      historial.innerHTML = historialTemp + "w";
+      historialTemp = historialTemp + "w";
+      console.log(numerosTemp);
+    }
+};
+  
+const btnLetrasEH = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "e";
+      historial.innerHTML = historialTemp + "e";
+      historialTemp = historialTemp + "e";
+      console.log(numerosTemp);
+    }
+};
+
+const btnLetrasRH = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "r";
+      historial.innerHTML = historialTemp + "r";
+      historialTemp = historialTemp + "r";
+      console.log(numerosTemp);
+    }
+};
+
+const btnLetrasTH = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "t";
+      historial.innerHTML = historialTemp + "t";
+      historialTemp = historialTemp + "t";
+      console.log(numerosTemp);
+    }
+};
+const btnLetrasYH = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "y";
+      historial.innerHTML = historialTemp + "y";
+      historialTemp = historialTemp + "y";
+      console.log(numerosTemp);
+    }
+};
+const btnLetrasIH = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "i";
+      historial.innerHTML = historialTemp + "i";
+      historialTemp = historialTemp + "i";
+      console.log(numerosTemp);
+    }
+};
+const btnLetrasOH = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "o";
+      historial.innerHTML = historialTemp + "o";
+      historialTemp = historialTemp + "o";
+      console.log(numerosTemp);
+    }
+};
+const btnLetrasPH = () => {
+    if (numerosTemp === "") {
+      casillas.innerHTML = numerosTemp + "p";
+      historial.innerHTML = historialTemp + "p";
+      historialTemp = historialTemp + "p";
+      console.log(numerosTemp);
+    }
+};
+
+//enter
+
+//enter 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//botones especiales
+const btnBorrarH = () => {
+  numerosTemp = "";
+  letrasTemp = "";
+  historialTemp = "";
+  casillas.innerHTML = "";
+};
+
+//botones numeros
+btnNum1.onclick = function () {
+  btnNum1H();
+};
+btnNum2.onclick = function () {
+    btnNum2H();
+};
+btnNum3.onclick = function () {
+    btnNum3H();
+};
+btnNum4.onclick = function () {
+    btnNum4H();
+};
+btnNum5.onclick = function () {
+    btnNum5H();
+};
+btnNum6.onclick = function () {
+    btnNum6H();
+};
+btnNum7.onclick = function () {
+    btnNum7H();
+};
+btnNum8.onclick = function () {
+    btnNum8H();
+};
+btnNum9.onclick = function () {
+    btnNum9H();
+};
+
+//botones letras
+
+btnLetrasQ.onclick = function () {
+    btnLetrasQH();
+};
+btnLetrasW.onclick = function () {
+    btnLetrasWH();
+};
+btnLetrasE.onclick = function () {
+    btnLetrasEH();
+};
+btnLetrasR.onclick = function () {
+    btnLetrasRH();
+};
+btnLetrasT.onclick = function () {
+    btnLetrasTH();
+};
+btnLetrasY.onclick = function () {
+    btnLetrasYH();
+};
+btnLetrasI.onclick = function () {
+    btnLetrasIH();
+};
+btnLetrasO.onclick = function () {
+    btnLetrasOH();
+};
+btnLetrasP.onclick = function () {
+    btnLetrasPH();
+};
+
+//enter
+
+//enter 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//borrar
+btnBorrar.onclick = function () {
+  btnBorrarH();
+};
